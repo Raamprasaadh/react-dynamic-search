620617fd6e5ea62c9b6d5fa0f602f28b3ee2c80a
raamprasaadh <ramprasadhsurya@gmail.com>
2022-06-16 10:17:38 +0530

    adds dsiplay of todo items

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import axios from 'axios';
 
 function App() {
+  const [data, setData]=React.useState([]);
+  React.useEffect(()=>{
+    // get details and store in the data using setData
+    axios.get("https://jsonplaceholder.typicode.com/todos")
+    .then((res)=>{setData(res.data)})
+    .catch((err)=>{console.error("error",err)})
+  },[])// to fetch details on first time only
   return (
     <div className="App">
+      
+      {
+        data.map(data=> <div key= {data.id}>
+          <div className="dataCard" style={(data.completed)?{backgroundColor:"green", color:"white"}:{backgroundColor:"red", color:"white"}}>
+            <p className="userId">{`ID: ${data.id}`}</p>
+            <p className="title">{`Title: ${data.title}`}</p>
+             <p className="completed">{`isCompleted: ${data.completed.toString()}`}</p>
+            </div>  
+        </div>
+          
+        )
+        
+      }
     </div>
   );
 }
